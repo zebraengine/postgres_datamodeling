7f1acbfa347fcd8e02cc31324f707aeae2f1385b
zebraengine <zebraengine@users.noreply.github.com>
2019-12-12 12:15:11 -0500

    missing s from "song"

--- postgres_datamodeling/sql_queries.py
+++ postgres_datamodeling/sql_queries.py
@@ -29,7 +29,7 @@ user_table_insert = (
     """INSERT INTO user (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s);""")
 
 song_table_insert = (
-    """INSERT INTO song (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s);""")
+    """INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s);""")
 
 artist_table_insert = (
     """INSERT INTO artist (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s);""")
